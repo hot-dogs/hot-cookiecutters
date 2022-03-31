e1883043be2eeb319e0438665650f553fd64233b
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-03-31 08:21:42 -1000

    added hot-ctd template

---
+++ projects/hot-ctd/hooks/pre_gen_project.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def validate():
+    creator = '{{cookiecutter.creator}}'
+
+    if not creator.strip():
+        print("ERROR FAILED(1): You must specify a creator to use this template")
+        return 1
+
+    if creator.lower().strip() == 'your name':
+        print("ERROR FAILED(2): You must specify a creator to use this template")
+        return 2
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(validate())
