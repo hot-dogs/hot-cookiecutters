c4819eba56eea82c7f703b5b3ae2b748d5219963
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-04-10 17:34:35 -1000

    mystyle.sty not working

--- projects/hot-ctd/hooks/pre_gen_project.py
+++ projects/hot-ctd/hooks/pre_gen_project.py
@@ -2,7 +2,7 @@ import sys
 
 
 def validate():
-    creator = '{{cookiecutter.creator}}'
+    creator = '{{cookiecutter.first_na,e}}'
 
     if not creator.strip():
         print("ERROR FAILED(1): You must specify a creator to use this template")
