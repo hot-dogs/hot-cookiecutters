449e400a166f448fa2efb46e7a18553c2add5978
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-04-11 20:39:30 -1000

    typo 2

--- projects/hot-ctd/hooks/pre_gen_project.py
+++ projects/hot-ctd/hooks/pre_gen_project.py
@@ -2,7 +2,7 @@ import sys
 
 
 def validate():
-    creator = '{{cookiecutter.first_name}}'
+    creator = '{{cookiecutter.processor_first_name}}'
 
     if not creator.strip():
         print("ERROR FAILED(1): You must specify a creator to use this template")
